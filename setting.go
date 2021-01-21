4dcbd98fb583a5ec78f83210ccb7897aab2f8ede
user <kiberlom>
2021-01-21 17:35:51 +0400

    second comment

--- setting.go
+++ setting.go
@@ -12,25 +12,21 @@ type SettingF struct {
 }
 
 // читаем настройки
-func setting() *SettingF{
+func setting() *SettingF {
 
-	// open the file pointer
 	sFile, err := os.Open("setting.json")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer sFile.Close()
 
-	// create a new decoder
 	sFileDecoder := json.NewDecoder(sFile)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// initialize the storage for the decoded data
 	s := &SettingF{}
 
-	// decode the data
 	err = sFileDecoder.Decode(&s)
 	if err != nil {
 		log.Fatal(err)
